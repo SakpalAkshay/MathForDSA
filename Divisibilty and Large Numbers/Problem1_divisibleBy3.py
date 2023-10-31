d4decabb3f795dd8c930bdb48c09d409ddb9b736
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-10-30 19:34:48 -0700

    Pushed Code

--- Divisibilty and Large Numbers/Problem1_divisibleBy3.py
+++ Divisibilty and Large Numbers/Problem1_divisibleBy3.py
@@ -1,1 +1,19 @@
+'''A number is divisible by 3 if sum of its digits is divisible by 3.'''
 
+def checkDiv(num) : 
+	
+	# Compute sum of digits 
+	digitSum = 0
+	while num > 0 : 
+		rem = num % 10
+		digitSum = digitSum + rem 
+		num = num // 10
+		
+	# divisible by 3. 
+	return (digitSum % 3 == 0) 
+	
+num = 1332
+if(checkDiv(num)) : 
+	print ("Yes") 
+else : 
+	print ("No") 
