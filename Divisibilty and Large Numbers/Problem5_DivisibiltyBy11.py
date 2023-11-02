b7f4b3c7986751f3b81c31d052044dde375200b3
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-11-01 19:04:01 -0700

    Added Code

--- Divisibilty and Large Numbers/Problem5_DivisibiltyBy11.py
+++ Divisibilty and Large Numbers/Problem5_DivisibiltyBy11.py
@@ -1,1 +1,26 @@
+'''
+A number is divisible by 11 if difference of following two is divisible by 11. 
+
+Sum of digits at odd places.
+Sum of digits at even places.
+'''
+
+
+n = '699210933779831'
+oddSum = 0
+evenSum = 0
+for i in range(0,len(n)):
+    
+    if i % 2 ==0:
+        evenSum += int(n[i])
+    else:
+        oddSum+= int(n[i])
+        
+
+
+if ((oddSum - evenSum)%11 ==0 ):
+    print("Yes")
+else:
+    print("No")
+    
 
