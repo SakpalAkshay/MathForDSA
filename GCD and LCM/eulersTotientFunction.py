16926984afaace752bb93c18464526ad1415a1c3
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-11-01 20:19:14 -0700

    Added code

--- GCD and LCM/eulersTotientFunction.py
+++ GCD and LCM/eulersTotientFunction.py
@@ -1,1 +1,16 @@
+n = 10
 
+def gcd(a,b):
+    while b!=0:
+        a,b = b, a%b
+    return a
+
+relativleyPrimeNumbers = []
+for i in range(1,n):
+    GCD = gcd(i,n)
+    
+    if GCD == 1:
+        relativleyPrimeNumbers.append(i)
+
+
+print(relativleyPrimeNumbers)
