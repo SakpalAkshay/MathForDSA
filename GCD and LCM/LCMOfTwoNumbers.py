d4e0be7af671dd5399f4d0f4c77068725b60d066
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-11-01 19:57:55 -0700

    Pushed code

--- GCD and LCM/LCMOfTwoNumbers.py
+++ GCD and LCM/LCMOfTwoNumbers.py
@@ -1,1 +1,19 @@
+a = 4
+b = 9
 
+def gcd(a,b):
+    
+    while b!=0:
+        a,b = b, a % b
+    return a
+
+
+
+def lcm(a,b):
+    GCD = gcd(a,b)
+    
+    return (a * b) // GCD
+
+LCM = lcm(a,b)
+
+print(LCM)
